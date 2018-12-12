90d17afdaac03df257b26f088bfb2941f87d938b
kabelo-sais <45555892+kabelo-sais@users.noreply.github.com>
2018-12-12 09:08:11 +0200

    Create collection-app-summary.component.ts

---
+++ src/app/views/cashoffice-reports/collection-app-summary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: 'collection-app-summary.component.html'
+})
+export class CollectionAppSummaryComponent {
+
+  constructor() { }
+
+}
