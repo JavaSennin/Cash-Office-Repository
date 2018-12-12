820cfe58089f16691165cf6ab5e55c5ab5b47cd2
kabelo-sais <45555892+kabelo-sais@users.noreply.github.com>
2018-12-12 09:09:35 +0200

    Create cashier-assignment.component.ts

---
+++ src/app/views/cashoffice-reports/cashier-assignment.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // selector: 'app-user-management',
+  templateUrl: './cashier-assignment.component.html'
+})
+export class CashierAssignmentComponent {
+
+  constructor() { }
+}
