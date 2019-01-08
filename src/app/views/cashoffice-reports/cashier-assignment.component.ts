911fc66239b8c1aa22cfe844cc9ea195577e8f9e
JavaSennin <iamt4@live.com>
2019-01-08 11:45:58 +0200

    commiting

--- src/app/views/cashoffice-reports/cashier-assignment.component.ts
+++ src/app/views/cashoffice-reports/cashier-assignment.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,NgModule, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms'; 
+
+@NgModule({
+  imports: [
+    FormControl,
+    FormGroup, 
+    FormsModule,
+    ReactiveFormsModule,
+    Validators
+]
+})
 
 @Component({
   // selector: 'app-user-management',
@@ -6,5 +17,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CashierAssignmentComponent {
 
-  constructor() { }
+  cashierInput = new FormGroup({
+    branchCode: new FormControl('', Validators.required),
+    cashOfficeCode: new FormControl('', Validators.required)
+  });
+
+  onSubmit(){
+    console.table(this.cashierInput.value) ;
+}
 }
--- src/app/views/cashoffice-reports/deposit-slip.component.ts
+++ src/app/views/cashoffice-reports/deposit-slip.component.ts
@@ -1,6 +1,16 @@
 // REPRINT DEPOSIT SLIP
 
-import { Component, OnInit } from '@angular/core';
+import { Component,NgModule, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms'; 
+
+
+@NgModule({
+  imports: [
+    FormControl,
+    FormGroup, 
+    FormsModule,
+    ReactiveFormsModule]
+})
 
 @Component({
   // selector: 'app-user-management',
@@ -8,5 +18,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepositSlipComponent {
 
-  constructor() { }
+  depositNumber = new FormControl('') ;
+
+  // reprintReceipt = new FormGroup({}) ;
+
+  viewReport(){
+    // this.receiptNumber.setValue('R' +  this.receiptNumber.value ) ;
+    console.log('Receipt No. ' + this.depositNumber.value) ;
+   }
+   exitReport(){
+    // this.receiptNumber.setValue('R' +  this.receiptNumber.value ) ;
+    console.log('Exit Report') ;
+   }
 }
