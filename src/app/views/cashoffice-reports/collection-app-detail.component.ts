24bd16167d86e4c72f9154db44b2af6dcd26a1e1
JavaSennin <iamt4@live.com>
2019-01-24 10:32:40 +0200

    app-detail

--- src/app/views/cashoffice-reports/collection-app-detail.component.ts
+++ src/app/views/cashoffice-reports/collection-app-detail.component.ts
@@ -21,11 +21,18 @@ export class CollectionAppDetailComponent {
     fromDate: new FormControl('2018-09-01', Validators.required),
     toDate: new FormControl('2018-09-30', Validators.required)
   });
+ 
 
   detailReport(){
+
+    this.displayReport = true ;
     console.table(this.detailInput.value) ;
 
     // form-processing code
   }
+  displayReport = false ;
 
+  toggleDisplayReport(){
+    this.displayReport = !this.displayReport ; // false
+  }
 }
