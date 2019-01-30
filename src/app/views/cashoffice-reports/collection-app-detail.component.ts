66f1e70ccc765276235ecdacff282641e066965a
JavaSennin <iamt4@live.com>
2019-01-30 12:39:06 +0200

    Collection App Detail

--- src/app/views/cashoffice-reports/collection-app-detail.component.ts
+++ src/app/views/cashoffice-reports/collection-app-detail.component.ts
@@ -35,8 +35,23 @@ export class CollectionAppDetailComponent {
   toggleDisplayReport(){
     this.displayReport = !this.displayReport ; // false
   }
-  //Array for Dummy data
+  //Array for Dummy data [Group Life System]
   reciepts: any[]= [
+    {reciept_number:1234,reciept_date:"27/09/67",amount:"258,896.00"},
+    {reciept_number:4567,reciept_date:"27/09/97",amount:"458,253.00"},
+    {reciept_number:4867,reciept_date:"02/09/97",amount:"488,253.00"},
+    {reciept_number:8897,reciept_date:"02/11/07",amount:"888,253.00"},
+    {reciept_number:9897,reciept_date:"09/11/07",amount:"1,888,253.00"}
+  ];
+   //Array for Dummy data
+   reciepts_policy: any[]= [
+    {reciept_number:1234,reciept_date:"27/09/67",amount:"258,896.00"},
+    {reciept_number:4567,reciept_date:"27/09/97",amount:"458,253.00"},
+    {reciept_number:4867,reciept_date:"02/09/97",amount:"488,253.00"},
+    {reciept_number:8897,reciept_date:"02/11/07",amount:"888,253.00"},
+    {reciept_number:9897,reciept_date:"09/11/07",amount:"1,888,253.00"}
+  ];
+  reciepts_gls: any[]= [
     {reciept_number:1234,reciept_date:"27/09/67",amount:"258,896.00"},
     {reciept_number:4567,reciept_date:"27/09/97",amount:"458,253.00"},
     {reciept_number:4867,reciept_date:"02/09/97",amount:"488,253.00"},
