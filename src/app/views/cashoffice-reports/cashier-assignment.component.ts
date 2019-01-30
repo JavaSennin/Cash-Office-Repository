8e249e53d0d52a0715aa8a082ba5b76583036a09
kabelo-sais <45555892+kabelo-sais@users.noreply.github.com>
2019-01-30 10:24:20 +0200

    Dynamo
    Dynamic data thinging thing

--- src/app/views/cashoffice-reports/cashier-assignment.component.ts
+++ src/app/views/cashoffice-reports/cashier-assignment.component.ts
@@ -33,4 +33,23 @@ export class CashierAssignmentComponent {
     this.displayReport = !this.displayReport ; // false
   }
 
+  // An Array to hold dynamic data - Payment Methods: 
+  paymentMethods = [ 
+    { code:"CSH", description:"Cash", start:"21-Jun-11",end:"" }, 
+    { code: "CHQ", description: "Cheque", start:"21-Jun-11",end:"" }
+  ] ;
+   
+  // An Array to hold dynamic data - Applications: 
+  applications = [ 
+    { code:"GPL", description:"Group Life System", start:"21-Jun-11",end:"" },
+    { code:"SUN", description:"Sundry Receipts", start:"21-Jun-11",end:"" },
+    { code:"TPP", description:"Thito Pay Point", start:"21-Jun-11",end:"" } 
+  ] 
+
+  // An Array to hold dynamic data - Cashiers
+  cashiers = [
+    {cashierID:"JDOE", cashierName:"Jane Doe", snrCashier:"N", start: "21-Jan-11", end: "30-Jun-12"},
+    {cashierID:"EPOE", cashierName:"Edgar Po", snrCashier:"Y", start: "21-Jun-11", end: "31-Aug-12"},
+    {cashierID:"DOZZ", cashierName:"Dawn Ozz", snrCashier:"Y", start: "21-Jun-11", end: "31-Dec-14"}
+  ] 
 }
