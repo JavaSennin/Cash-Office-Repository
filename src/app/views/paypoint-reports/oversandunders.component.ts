ae086d9e45f7bdf5cf84ec7af384f333d4351d1d
JavaSennin <iamt4@live.com>
2019-02-06 11:02:14 +0200

    Overs and Unders

--- src/app/views/paypoint-reports/oversandunders.component.ts
+++ src/app/views/paypoint-reports/oversandunders.component.ts
@@ -7,7 +7,8 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
     FormGroup, 
     FormsModule,
     ReactiveFormsModule,
-    Validators]
+    Validators
+]
 })
 
 @Component({
@@ -15,30 +16,46 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
 })
 export class OversandUndersComponent {
 
-  cashierInput = new FormGroup({
-  branchCode: new FormControl('', Validators.required)
-  
+  detailInput = new FormGroup({
+    branchCode: new FormControl('', Validators.required),
+    fromDate: new FormControl('2018-09-01', Validators.required),
+    toDate: new FormControl('2018-09-30', Validators.required)
   });
-  onSubmit(){
-    this.displayReport = true ; // show container for the results. 
-  
-    console.table(this.cashierInput.value) ;
-  }
+ 
 
-  
-  
+  detailReport(){
+
+    this.displayReport = true ;
+    console.table(this.detailInput.value) ;
+
+    // form-processing code
+  }
   displayReport = false ;
 
   toggleDisplayReport(){
     this.displayReport = !this.displayReport ; // false
   }
-
-  //Array for Dummy data
+  //Array for Dummy data [Group Life System]
   reciepts: any[]= [
-    {s_no:"",reciept_number:1234,reciept_date:"27/09/67",amount:"258,896.00"},
-    {s_no:"",reciept_number:4567,reciept_date:"27/09/97",amount:"458,253.00"},
-    {s_no:"",reciept_number:4867,reciept_date:"02/09/97",amount:"488,253.00"},
-    {s_no:"",reciept_number:8897,reciept_date:"02/11/07",amount:"888,253.00"},
-    {s_no:"",reciept_number:9897,reciept_date:"09/11/07",amount:"1,888,253.00"}
+    {reciept_number:1234,reciept_date:"27/09/67",amount:"258,896.00"},
+    {reciept_number:4567,reciept_date:"27/09/97",amount:"458,253.00"},
+    {reciept_number:4867,reciept_date:"02/09/97",amount:"488,253.00"},
+    {reciept_number:8897,reciept_date:"02/11/07",amount:"888,253.00"},
+    {reciept_number:9897,reciept_date:"09/11/07",amount:"1,888,253.00"}
+  ];
+   //Array for Dummy data
+   reciepts_policy: any[]= [
+    {reciept_number:1234,reciept_date:"27/09/67",amount:"258,896.00"},
+    {reciept_number:4567,reciept_date:"27/09/97",amount:"458,253.00"},
+    {reciept_number:4867,reciept_date:"02/09/97",amount:"488,253.00"},
+    {reciept_number:8897,reciept_date:"02/11/07",amount:"888,253.00"},
+    {reciept_number:9897,reciept_date:"09/11/07",amount:"1,888,253.00"}
+  ];
+  reciepts_gls: any[]= [
+    {reciept_number:1234,reciept_date:"27/09/67",amount:"258,896.00"},
+    {reciept_number:4567,reciept_date:"27/09/97",amount:"458,253.00"},
+    {reciept_number:4867,reciept_date:"02/09/97",amount:"488,253.00"},
+    {reciept_number:8897,reciept_date:"02/11/07",amount:"888,253.00"},
+    {reciept_number:9897,reciept_date:"09/11/07",amount:"1,888,253.00"}
   ];
 }
