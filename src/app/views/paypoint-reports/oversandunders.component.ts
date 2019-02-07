46ef09bba1922623191a5f3b3817a376b6d47baf
JavaSennin <iamt4@live.com>
2019-02-07 14:55:57 +0200

    Overs and Unders Is Done

--- src/app/views/paypoint-reports/oversandunders.component.ts
+++ src/app/views/paypoint-reports/oversandunders.component.ts
@@ -18,7 +18,7 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators, F
 export class OversandUndersComponent {
 
   detailInput = new FormGroup({
-    PayPointID: new FormControl({value:"", disabled: false}, Validators.required),
+    PayPointID: new FormControl('', Validators.required),
     
     Paypoint_Name: new FormControl({value:"", disabled: true}, Validators.required),
     Period: new FormControl('2018-09-30', Validators.required)
@@ -26,9 +26,9 @@ export class OversandUndersComponent {
  
 
   detailReport(){
-
+    this.detailInput.disable() ;
     this.displayReport = true ;
-    this.disableForm = true;
+    
     console.table(this.detailInput.value) ;
 
     // form-processing code
@@ -39,9 +39,13 @@ export class OversandUndersComponent {
 
   toggleDisplayReport(){
     this.displayReport = !this.displayReport ; // false
-    this.disableForm = !this.disableForm ; // false
-
+    
+    this.detailInput.enable() ;
+    this.detailInput.get('Paypoint_Name').disable();
+    
+    
   }
+  
   // paypointIds: any[]= [
   paypointIds = [ 
     {ppID:1234, ppName: "Botswana Railways"},
@@ -55,26 +59,21 @@ export class OversandUndersComponent {
 
   //Array for Dummy data [Group Life System]
   ppReport: any[]= [
-    {pp_id:1234,reciept_date:"27/09/67",amount:"258,896.00"},
-    {pp_id:4567,reciept_date:"27/09/97",amount:"458,253.00"},
-    {pp_id:4867,reciept_date:"02/09/97",amount:"488,253.00"},
-    {pp_id:8897,reciept_date:"02/11/07",amount:"888,253.00"},
-    {pp_id:9897,reciept_date:"09/11/07",amount:"1,888,253.00"}
+    {pp_id:1234,ppType:"Employer",pMode:"ESO(Semi-Electronic"}
   ];
    //Array for Dummy data
-   reciepts_policy: any[]= [
-    {reciept_number:1234,reciept_date:"27/09/67",amount:"258,896.00"},
-    {reciept_number:4567,reciept_date:"27/09/97",amount:"458,253.00"},
-    {reciept_number:4867,reciept_date:"02/09/97",amount:"488,253.00"},
-    {reciept_number:8897,reciept_date:"02/11/07",amount:"888,253.00"},
-    {reciept_number:9897,reciept_date:"09/11/07",amount:"1,888,253.00"}
+   ppReport2: any[]= [
+    {collectionType:"Debits:",Total:234456.67},
+    {collectionType:"Credits:",Total:0}
+    
   ];
-  reciepts_gls: any[]= [
-    {reciept_number:1234,reciept_date:"27/09/67",amount:"258,896.00"},
-    {reciept_number:4567,reciept_date:"27/09/97",amount:"458,253.00"},
-    {reciept_number:4867,reciept_date:"02/09/97",amount:"488,253.00"},
-    {reciept_number:8897,reciept_date:"02/11/07",amount:"888,253.00"},
-    {reciept_number:9897,reciept_date:"09/11/07",amount:"1,888,253.00"}
+  ppReport3: any[]= [
+    {ref_no:"00",ref_name:"Nonofo Odubegile",policy_code:"445889600",status:"inforce",damount:9211.58,aamount:0.00,difamount:2121.58},
+    {ref_no:"00",ref_name:"Patricia Majalisa",policy_code:"345889600",status:"inforce",damount:2811.58,aamount:0.00,difamount:2211.58},
+    {ref_no:"00",ref_name:"Lucky Dube Lance",policy_code:"2589889600",status:"inforce",damount:2111.58,aamount:0.00,difamount:21221.58},
+    {ref_no:"00",ref_name:"Freid Jokovic",policy_code:"75889600",status:"inforce",damount:2211.58,aamount:0.00,difamount:2121.58},
+    {ref_no:"00",ref_name:"Mila Stuart",policy_code:"55889600",status:"inforce",damount:4211.58,aamount:0.00,difamount:2211.58},
+    
   ];
 
   // bgn
