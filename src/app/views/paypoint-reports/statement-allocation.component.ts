e265ddcfe6efe2aa6bcbf323e67eeb9bb989f70c
kabelo-sais <45555892+kabelo-sais@users.noreply.github.com>
2019-02-13 17:00:52 +0200

    header content

--- src/app/views/paypoint-reports/statement-allocation.component.ts
+++ src/app/views/paypoint-reports/statement-allocation.component.ts
@@ -20,6 +20,8 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators, F
 export class StatementAllocationComponent {
 
   today = new Date() ;
+  displayReport = false ;
+  disableForm = false;
 
   detailInput = new FormGroup({
     PayPointID: new FormControl('', Validators.required),
@@ -37,10 +39,7 @@ export class StatementAllocationComponent {
 
     // form-processing code
   }
-  displayReport = false ;
-
-  disableForm = false;
-
+  
   toggleDisplayReport(){
     this.displayReport = !this.displayReport ; // false
     
@@ -69,4 +68,13 @@ export class StatementAllocationComponent {
   totalAmount: number = 
     this.allocations.reduce( function(accumulator, currentValue){ return accumulator +  currentValue.amount}, 0 ) ;
   
+  // Array to hold Dynamic Data - Bank Statement Details
+  statement = // [
+    { stmtID: "210", bankName: "First Nation Bank", stmtNo: "FNB42/12012019", accNo: "4638001", 
+      accDesc: "DDE Collection Account", period: "21-Jan-2017", strikeDate: "21-Mar-17", allocatedAmount: 451.05} ; //,
+  //   { stmtID: "321", bankName: "Captial Bank", stmtNo: "CPB43/12012019", accNo: "4638002", 
+  //     accDesc: "BSO Collection Account", period: "21-Jun-2017", strikeDate: "21-Aug-17", alllocatedAmount: 452.14},
+  //   { stmtID: "432", bankName: "Bank Tlokweng", stmtNo: "BTL44/12012019", accNo: "4638007", 
+  //     accDesc: "DDE Collection Account", period: "21-Sep-2017", strikeDate: "21-Nov-17", alllocatedAmount: 455.44}
+  // ];
 }
