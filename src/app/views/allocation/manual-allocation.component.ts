6ee03967d371ef8354ed6f791847ddb409932d9c
JavaSennin <iamt4@live.com>
2019-02-21 15:31:09 +0200

    Commit

--- src/app/views/allocation/manual-allocation.component.ts
+++ src/app/views/allocation/manual-allocation.component.ts
@@ -20,13 +20,30 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
 export class ManualAllocationComponent {
 
   
-  cashierInput = new FormGroup({
-    branchCode: new FormControl('', Validators.required),
-    cashOfficeCode: new FormControl('', Validators.required)
+  ManualAll = new FormGroup({
+    receiptNumber: new FormControl('', Validators.required),
+    receiptNumberLa: new FormControl('', Validators.required)
   });
 
   onSubmit(){
-    console.table(this.cashierInput.value) ;
+    this.displayReport = true;
+    console.table(this.ManualAll.value) ;
 }
+
+displayReport = false;
+
+recNo=[
+  {receNo:"Bank Statement"},
+  {receNo:"Reciept Number"}
+  
+];
+table1data=[
+  {pID:"7985236",per:"12/03/2019",gamount:2345.45,ramount:123.89,aamount:345.53,uamount:897.55}
+  
+];
+table2data=[
+  {pCode:"7985236",per:"12/03/2019",pID:123451,eamount:234.98,purpose:"IDK",purposeid:2445,pstatus:"Posted",aamount:235.23}
+  
+];
 
 }
