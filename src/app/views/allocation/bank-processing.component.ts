0a43780ba3e3f2a09825a2e7fd034118858add91
JavaSennin <iamt4@live.com>
2019-02-25 11:32:05 +0200

    Bank Stop Oder

--- src/app/views/allocation/bank-processing.component.ts
+++ src/app/views/allocation/bank-processing.component.ts
@@ -16,14 +16,26 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
 })
 export class BankProcessingComponent {
 
-  summaryInput = new FormGroup({
-    branchCode: new FormControl('', Validators.required),
-    fromDate: new FormControl('2018-09-01', Validators.required),
-    toDate: new FormControl('2018-09-30', Validators.required)
+  BankStatInput = new FormGroup({
+    paymentMode: new FormControl('', Validators.required),
+    Bdid: new FormControl('', Validators.required),
+    BName: new FormControl('', Validators.required),
+    accNumber: new FormControl('', Validators.required),
+    CrDate: new FormControl('', Validators.required),
+    mdDate: new FormControl('', Validators.required),
+    AccDesc: new FormControl('', Validators.required),
+    statementNumber: new FormControl('', Validators.required),
+    postingStatus: new FormControl('UNPOSTED', Validators.required),
+    dateFrom: new FormControl('', Validators.required),
+    dateTo: new FormControl('', Validators.required),
+    loginName: new FormControl('tkadimo', Validators.required),
+    opBalance: new FormControl('', Validators.required),
+    closingBalance: new FormControl('', Validators.required),
+    Branch: new FormControl('Gaborone HQ', Validators.required),
   });
 
   summaryReport(){
-    console.table(this.summaryInput.value) ;
+    console.table(this.BankStatInput.value) ;
 
     // form-processing code
   }
