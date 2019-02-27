c1f850c8d012b3acb447b9b871ab91218d4ebdfa
kabelo-sais <45555892+kabelo-sais@users.noreply.github.com>
2019-02-27 09:20:09 +0200

    Editable Table

--- src/app/views/allocation/direct-debit.component.ts
+++ src/app/views/allocation/direct-debit.component.ts
@@ -50,6 +50,44 @@ export class DirectDebitComponent {
 
   }) ;
 
+  // bgn: Editable Table
+  editField: string;
+  personList: Array<any> = [
+    { id: "1", name: 'Aurelia Vega', age: 30, companyName: 'Deepends', country: 'Spain', city: 'Madrid' }
+    // { id: 2, name: 'Guerra Cortez', age: 45, companyName: 'Insectus', country: 'USA', city: 'San Francisco' },
+    // { id: 3, name: 'Guadalupe House', age: 26, companyName: 'Isotronic', country: 'Germany', city: 'Frankfurt am Main' },
+    // { id: 4, name: 'Aurelia Vega', age: 30, companyName: 'Deepends', country: 'Spain', city: 'Madrid' },
+    // { id: 5, name: 'Elisa Gallagher', age: 31, companyName: 'Portica', country: 'United Kingdom', city: 'London' },
+  ];
+
+  awaitingPersonList: Array<any> = [
+    { id: 6, name: 'George Vega', age: 28, companyName: 'Classical', country: 'Russia', city: 'Moscow' },
+    { id: 10, name: 'John Maklowicz', age: 36, companyName: 'Mako', country: 'Poland', city: 'Bialystok' },
+  ];
+
+  
+
+  updateList(id: number, property: string, event: any) {
+    const editField = event.target.textContent;
+    this.personList[id][property] = editField;
+  }
+
+  remove(id: any) {
+    this.awaitingPersonList.push(this.personList[id]);
+    this.personList.splice(id, 1);
+  }
+
+  default = false ;
+  add() {
+      this.personList[0]={ id: 21, name: 'PersonName', age: 21, companyName: 'Company', country: 'Country', city: 'City' };
+      this.default = true ;
+  }
+
+  changeValue(id: number, property: string, event: any) {
+    this.editField = event.target.textContent;
+  }
+  // end: Editable Table
+
   displayReport = false ;
   disableForm = false;
 
