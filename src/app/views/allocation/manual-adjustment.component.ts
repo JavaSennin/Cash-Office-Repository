7d3c412f21f518b99f083d033638f8899a4a76ef
kabelo-sais <45555892+kabelo-sais@users.noreply.github.com>
2019-03-01 17:02:29 +0200

    dd

--- src/app/views/allocation/manual-adjustment.component.ts
+++ src/app/views/allocation/manual-adjustment.component.ts
@@ -12,17 +12,9 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
 })
 
 @Component({
-  // selector: 'app-user-management',
   templateUrl: './manual-adjustment.component.html'
 })
 export class ManualAdjustmentComponent {
-
-  listingInput = new FormGroup({
-    branchCode: new FormControl('', Validators.required),
-    cashOfficeCode: new FormControl('', Validators.required),
-    cashierCode: new FormControl('', Validators.required),
-    reportDate: new FormControl('2018-09-30', Validators.required)
-  });
 
   // bgn: import rsp-tab
 
@@ -32,42 +24,77 @@ export class ManualAdjustmentComponent {
   });
   
   onSubmit(){
-    this.unspecifiedInput.disable() ;
+    // this.unspecifiedInput.disable() ;
 
-    this.displayReport = true ; // show container for the results
+    // this.displayReport = true ; // show container for the results
   
-    console.table(this.unspecifiedInput.value) ;
+    // console.table(this.unspecifiedInput.value) ;
 
   }
 
   displayReport = false ;
 
   toggleDisplayReport(){
-    this.displayReport = !this.displayReport ; // false
-    this.unspecifiedInput.enable() ;
+    // this.displayReport = !this.displayReport ; // false
+    // this.unspecifiedInput.enable() ;
   }
       
+
+  // bgn: working items
+
   today = new Date() ; 
 
-  // An Array to hold dynamic data - Manual Adjustments:
+  newTransInput = new FormGroup({
+    policyCode: new FormControl('', Validators.required),
+    amount: new FormControl('', Validators.required),
+    period: new FormControl('2018-09-01', Validators.required),
+    transType: new FormControl('', Validators.required),
+    comments: new FormControl('', Validators.required),
+
+    bnkStmtNo: new FormControl({value:'', disabled: true})
+
+  });
+
+  radios = new FormControl('', Validators.required)  ;
+
+  // An Array to hold dynamic data - Manual Adjustments:  
   adjustments = [
     {
       mnAdjID: 20, policyCode: "100210611", transType: "Premium Allocation", policyID: "210611", 
-      period: "21-Jun-2016", payerName: "A. B. Doe", payerNo: 14234, amount: 281.56, comments: "Unprocessed Allocation", purpose: 2, postingStatus: "POSTED", bobiRefNo: 123456, creation: "21-Jun-2012"
+       comments: "Unprocessed Allocation", purpose: 2, postingStatus: "POSTED", bobiRefNo: 123456, creation: "21-Jun-2012"
     },
     {
       mnAdjID: 14, policyCode: "100210617", transType: "Premium Allocation", policyID: "210617", 
-      period: "21-Jan-2017", payerName: "C. D. Doe", payerNo: 15234, amount: 291.56, comments: "Review the Allocation", purpose: 4, postingStatus: "UNPOSTED", bobiRefNo: 123457, creation: "21-Jun-2013"
+       comments: "Review the Allocation", purpose: 4, postingStatus: "UNPOSTED", bobiRefNo: 123457, creation: "21-Jun-2013"
     },
     {
       mnAdjID: 12, policyCode: "100210618", transType: "Premium Allocation", policyID: "210618", 
-      period: "21-Aug-2018", payerName: "E. F. Doe", payerNo: 16234, amount: 201.56, comments: "Unprocessed Allocation", purpose: 5, postingStatus: "POSTED", bobiRefNo: 123458, creation: "21-Jun-2014"
+       comments: "Unprocessed Allocation", purpose: 5, postingStatus: "POSTED", bobiRefNo: 123458, creation: "21-Jun-2014"
     }
   ]
 
-  totalAmount: number = 
-    this.adjustments.reduce( function(accumulator, currentValue){ return accumulator +  currentValue.amount}, 0 ) ;
+  // Record for Posting Controls
+  onSelect(selectedItem: any, x: any) {
+    console.log("Selected item Id: " + selectedItem.policyCode); // You get the Id of the selected item here
+    console.log("the items is at row " + x) ;
+}
 
+  // New Transaction Controls
+  clear(){
+
+  }
+
+  exit(){ this.clear() ;  }
+
+  post(){
+
+  }
+
+  save(){
+
+  }
+
+  // end: working items
 
   // end: import rsp-tab
 
