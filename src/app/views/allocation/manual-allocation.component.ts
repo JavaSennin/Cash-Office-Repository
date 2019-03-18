e68dfbd0d1b17d0c6cde39b9aa40d4ea8c49deea
JavaSennin <iamt4@live.com>
2019-03-18 09:43:38 +0200

    Manual Allocation

--- src/app/views/allocation/manual-allocation.component.ts
+++ src/app/views/allocation/manual-allocation.component.ts
@@ -1,7 +1,9 @@
-// TO-DO: vlaueChanges subsription for receiptNumber field
+// Partial MisAllocation Correction - Allocation Module
 
-import { Component, OnInit, NgModule } from '@angular/core';
+import { Component,NgModule, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms'; 
+
+import { FormBuilder, FormArray } from '@angular/forms'; // form array things require FormGroup as well
 
 @NgModule({
   imports: [
@@ -10,51 +12,124 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
     FormsModule,
     ReactiveFormsModule,
     Validators
+
+    , FormArray, FormBuilder
 ]
 })
 
 @Component({
-  // selector: 'reprint-receipt',
   templateUrl: './manual-allocation.component.html'
 })
 export class ManualAllocationComponent {
-
-  
-  ManualAll = new FormGroup({
-    receiptNumber: new FormControl('', Validators.required),
-    receiptNumberLa: new FormControl('', Validators.required)
-  });
-
-  onSubmit(){
-    this.displayReport = true;
-    console.table(this.ManualAll.value) ;
-}
-
-showAll(){
-  this.displayReport1 = true;
  
+  viewMisallocations = false ;
+  makeCorrections = true ; // show editable table for "Premium Reallocation" transactions.
+
+  toggleMakeCorrections(transaction_type){
+    if ( transaction_type == 'allocate')
+    {
+      this.makeCorrections = true ;
+    }
+
+    if ( transaction_type == 'reverse')
+    {
+      this.makeCorrections = false ;
+    }
+  }
+
+  search(x){
+    console.log("Searching " + x) ;
+    this.viewMisallocations = true ;
+  }
+    
+  misallocatedInput = new FormGroup({
+    receiptNo: new FormControl('', Validators.required),
+    BSFlag: new FormControl("2018-09-01", Validators.required),
+
+    misallocationID: new FormControl({value: '', disabled: true}),
+
+    radios: new FormControl(''), // radio button things
+
+    transType: new FormControl('') // radio button things
+  }) ;
+
+// An Array to hold dynamic data - Misallocations:  
+misallocations = [
+  {
+    collID: 20, policyCode: "210611", policyStatus: "Terminated", period: "2012-06-21",
+     payer: "Jane Doe", amount: 432.11, postingStatus: "T", receiptNo: 123456
+  },
+  {
+    collID: 14, policyCode: "210617", policyStatus: "Terminated", period: "2013-06-21",
+     payer: "John Doe", amount: 114.32, postingStatus: "U", receiptNo: 123457
+  },
+  {
+    collID: 12, policyCode: "210618", policyStatus: "Terminated", period: "2014-06-21",
+     payer: "Joanne Odin", amount: 413.12, postingStatus: "V", receiptNo: 123458
+  }
+]
+
+selectedItem: any ; // placeholder for a specific collection item
+ 
+onSelect(x, position) 
+{
+  this.selectedItem = x ;
+  console.log("Selected item No. " + ( position +1 ) ) ; // dbg
 }
 
-displayReport = false;
-displayReport1 = false;
-
-toggleDisplayReport(){
-  this.displayReport = !this.displayReport ; // false
-  this.displayReport1 = !this.displayReport1 ; // false
+clear(){
+  window.location.reload() ;
 }
 
-recNo=[
-  {receNo:"Bank Statement"},
-  {receNo:"Reciept Number"}
-  
-];
-table1data=[
-  {pID:"7985236",per:"12/03/2019",gamount:2345.45,ramount:123.89,aamount:345.53,uamount:897.55}
-  
-];
-table2data=[
-  {pCode:"7985236",per:"12/03/2019",pID:123451,eamount:234.98,purpose:"IDK",purposeid:2445,pstatus:"Posted",aamount:235.23}
-  
-];
+exit(){
+  // Re-direct to app landing page
+  window.location.href = "http://localhost:4200/#/dashboard" ;
+}
+
+save(){}
+
+myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      corrections: this.fb.array([])
+    })
+
+  }
+
+  get correctionForms() {
+    return this.myForm.get('corrections') as FormArray
+  }
+
+  addCorrection() {
+
+    const correction = this.fb.group({ 
+      corSelect:[],
+      corPolicyCode: [],
+      corPeriod: [],
+      corPartyID: [],
+      corPayerName: [],
+      corExpectedAmnt: [],
+      corPurpose: [],
+      corAllocatedAmnt: []
+      
+    })
+
+    this.correctionForms.push(correction);
+  }
+
+  deleteCorrection(i) {
+    this.correctionForms.removeAt(i)
+  }
+
+  post(){
+    console.table(this.myForm.get('corrections').value) ; // dbg
+
+    // other form-processing code here:
+  }
+
+  totalAllocatedAmount: number ;
 
 }
