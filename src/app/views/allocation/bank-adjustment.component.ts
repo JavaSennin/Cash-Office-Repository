65548bfdb8590dbe43ffda570c05938dab4e4635
kabelo-sais <45555892+kabelo-sais@users.noreply.github.com>
2019-03-19 12:56:49 +0200

    html content for acc & stmt

--- src/app/views/allocation/bank-adjustment.component.ts
+++ src/app/views/allocation/bank-adjustment.component.ts
@@ -1,7 +1,9 @@
 // Bank Statement Adjustment Voucher - Allocation Module
 
 import { Component, NgModule } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms'; 
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { FormBuilder, FormArray } from '@angular/forms'; // form array things require FormGroup as well
 
 @NgModule({
   imports: [
@@ -9,7 +11,9 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
     FormGroup, 
     FormsModule,
     ReactiveFormsModule,
-    Validators]
+    Validators
+  
+  , FormArray, FormBuilder]
 })
 
 @Component({
@@ -17,9 +21,48 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
 })
 export class BankAdjustmentComponent {
 
-  branchCode = new FormControl('', Validators.required) ;
+  bankStatementID = new FormControl('', Validators.required) ;
 
-  viewBranchReport(){
-    console.log('Branch Code: ' + this.branchCode.value) ;
+  bnkAccDetails = new FormGroup({
+    paymentMode: new FormControl({value:'', disabled: true}),
+    creationDate: new FormControl({value:"2018-09-01", disabled: true}),
+    modifiedDate: new FormControl({value:"2018-09-30", disabled: true}),
+
+    bankName: new FormControl({value:'', disabled: true}),
+    accountNo: new FormControl({value:'', disabled: true}),
+    accountDesc: new FormControl({value:'', disabled: true})
+
+  }) ;
+
+  searchSucceeded: boolean = false ;
+
+  clear(){
+    // clearing code here: 
+
+    this.searchSucceeded = false ; // hide control buttons
   }
+
+  search(x){
+    console.log('Searching ' + x) ; // dbg
+
+    // form-processing code here: 
+
+    this.searchSucceeded = true ; // show control buttons
+  }
+
+  bnkStmtDetails = new FormGroup({
+    statementNo: new FormControl({value: '', disabled: true}),
+    fromDate: new FormControl({value: '2018-09-01', disabled: true }),
+    openingBalance: new FormControl({value: '', disabled: true}),
+
+    postingStatus: new FormControl({value:'', disabled: true}),
+    toDate: new FormControl({value: '2018-09-30', disabled: true }),
+    closingBalance: new FormControl({value: '', disabled: true}),
+
+    glPostingStatus: new FormControl({value:'', disabled: true}),
+    loginName: new FormControl({value: '', disabled: true}),
+    branch: new FormControl({value: '', disabled: true})
+
+  }) ;
+
 }
