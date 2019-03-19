fe85dea8ca76379c082d1a7b1ef7808ce64e92f0
JavaSennin <iamt4@live.com>
2019-03-19 11:53:36 +0200

    Paypoint Allocation

--- src/app/views/allocation/paypoint-misallocation.component.ts
+++ src/app/views/allocation/paypoint-misallocation.component.ts
@@ -17,12 +17,40 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
 })
 export class PaypointMisallocationComponent {
  
-  cashierInput = new FormGroup({
-    branchCode: new FormControl('', Validators.required),
-    cashOfficeCode: new FormControl('', Validators.required)
+  paypointMis = new FormGroup({
+    rid: new FormControl('', Validators.required),
+
   });
 
+  paypointMis2 = new FormGroup({
+    rNo: new FormControl('', Validators.required),
+    gAmnt: new FormControl('', Validators.required),
+    pp: new FormControl('', Validators.required),
+    pp1: new FormControl('', Validators.required),
+    period: new FormControl("", Validators.required),
+
+  });
+
+  paypointMis3 = new FormGroup({
+    ppnt: new FormControl('', Validators.required),
+    ppName: new FormControl('', Validators.required),
+    period2: new FormControl('', Validators.required),
+    crF: new FormControl('', Validators.required),
+    period: new FormControl("", Validators.required),
+
+  });
+  
+  paypoints = [
+    {pID:234,pName:"Botswana Railways"},
+    {pID:834,pName:"Botswana Post"},
+  ]
+
+  creditF = [
+    {fName:"inquisition.csv",tcfa:170000.89},
+    {fName:"0045Cr234rt.csv",tcfa:890000.78},
+  ]
+
   onSubmit(){
-    console.table(this.cashierInput.value) ;
+    console.table(this.paypointMis.value) ;
 }
 }
