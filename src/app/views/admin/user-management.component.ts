5edec5af9586bc8d6f20da8a3bcd06814199f84f
kabelo-sais <45555892+kabelo-sais@users.noreply.github.com>
2019-03-27 15:58:45 +0200

    User-mgt

--- src/app/views/admin/user-management.component.ts
+++ src/app/views/admin/user-management.component.ts
@@ -19,21 +19,7 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
   templateUrl: './user-management.component.html'
 })
 export class UserManagementComponent {
-
-  // createCashier: FormGroup;
-  // constructor(private http: HttpClient) {
-  // constructor() {
-  //   this.createCashier = new FormGroup(
-  //     {
-  //       cashierCode: new FormControl(),
-  //       cashierName: new FormControl(),
-  //       loginId: new FormControl(),
-  //       branchCode: new FormControl(),
-  //       branchName: new FormControl({ value: '', disabled: true })
-  //     }
-  //   );
-  // }
-
+  
   userInput = new FormGroup({
     username: new FormControl('', Validators.required),
     password: new FormControl('', Validators.required),
@@ -44,15 +30,44 @@ export class UserManagementComponent {
     )
   }) ;
 
-  assignGroup(){}
+  groupInput = new FormGroup({
+    username: new FormControl({value:'', disabled:true}, Validators.required),
+    firstName: new FormControl({value:'', disabled:true}),
+    lastName: new FormControl({value:'', disabled:true}),
+    groupName: new FormControl('', Validators.required),
+    groupDesc: new FormControl({value:'', disabled:true}),
+    enabled: new FormControl({value:'', disabled:true}),
+  }) ;
+
+  viewUserGroups = false ;
+  viewUserMgt = true ;
+
+  assignGroup(){
+    this.viewUserMgt = false ;
+    this.viewUserGroups = true ; 
+    this.userInput.disable() ;
+
+    // other assignGroup code here
+  }
 
   clear(){
     this.userInput.reset() ;
   }
 
+  clearUserGroups(){
+    this.groupInput.reset() ;
+  }
+
   exit(){
     // Re-direct to app landing page
     window.location.href = "http://localhost:4200/#/dashboard" ;
+  }
+
+  exitUserGroups(){
+    this.viewUserGroups = false ;
+    this.viewUserMgt = true ;
+
+    this.userInput.enable() ;
   }
 
   find(){}
@@ -61,10 +76,22 @@ export class UserManagementComponent {
 
   save(){}
 
+  saveUserGroups(){}
+
   // Array to hold dynamic data - Users
   users = [
-    { username: "ioamu,", password: "ioamu221", firstName: "Yioagos", lastName: "Avraamu", enabled: "false"},
+    { username: "ioamu", password: "ioamu221", firstName: "Yioagos", lastName: "Avraamu", enabled: "false"},
     { username: "amios", password: "amios332", firstName: "Avram", lastName: "Tarasios", enabled: "true"},
     { username: "inard", password: "inard2341", firstName: "Quitin", lastName: "Edward", enabled: "false"}
   ]
+
+  // Array to hold dynamic data - User Groups
+  userGroups = [
+    { groupName: "CAS", groupDesc: "Cashier", firstName: "Yioagos", lastName: "Avraamu", enabled: "false"},
+    { groupName: "SRC", groupDesc: "Senior Cashier", firstName: "Avram", lastName: "Tarasios", enabled: "true"},
+    { groupName: "CAS", groupDesc: "Cashier", firstName: "Quitin", lastName: "Edward", enabled: "false"}
+  ]
+
+  // Array to hold dynamic data - Assignee
+  assignee = {usn: "jhnvn", fsn: "John", lsn: "von Neumann",  gpn: "SRC", gds: "Senior Cashier", enb: 'true'} ;
 }
