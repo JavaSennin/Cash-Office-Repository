e53a3e6c6477f1eb15dcb741b8b6408224a3478d
kabelo-sais <45555892+kabelo-sais@users.noreply.github.com>
2019-05-08 14:23:05 +0200

    Nestle => Display nested json repsonce.

--- src/app/views/cashoffice-reports/collection-app-summary.component.ts
+++ src/app/views/cashoffice-reports/collection-app-summary.component.ts
@@ -5,6 +5,8 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import * as _ from 'underscore' ; /// npm install underscore
+
 @NgModule({
   imports: [
     FormControl,
@@ -26,6 +28,8 @@ export class CollectionAppSummaryComponent {
     toDate: new FormControl('2018-09-30', Validators.required)
   });
 
+  boxOffice : boolean = false ; 
+
   branchCodes : any
   branchName: string = "" ;  
 
@@ -34,18 +38,21 @@ export class CollectionAppSummaryComponent {
   receipts : any ;
   receipts_gls : any ; // Group Life System
   receipts_sun : any ; // Sundry Receipts
-  receipts_tpol : any ; // 
+  receipts_tpol : any ; // Polity
+  receipts_thito : any ; /// Thito Pay Point 
 
   // Collection Receipt Counters
   totalGCount : number = 0 ; 
   totalPCount : number = 0 ; 
-  totalSCount : number = 0 ; 
+  totalSCount : number = 0 ;
+  totalTPPC : number = 0 ; /// tpp 
 
   // Collection Amount Totals
   totalBranch: number = 0.0 ; 
   totalGls: number = 0.0 ; 
   totalSun: number = 0.0 ; 
   totalTPOL: number = 0.0 ; 
+  totalTPP : number = 0.0 ; /// tpp
   
   url : string;
 
@@ -65,6 +72,8 @@ export class CollectionAppSummaryComponent {
 
     }
     ,err => this.handleError(err));
+
+    this.showGroupies() ; /// dbg
   }
 
   detailReport(){
@@ -110,20 +119,61 @@ export class CollectionAppSummaryComponent {
     this.receipts_sun = this.filterApp(this.receipts, "SUNDRY") ;
     this.receipts_tpol = this.filterApp(this.receipts, "POLICY") ;
 
+    this.receipts_thito = this.filterApp(this.receipts, "THITO") ; /// tpp
+    console.log(this.receipts_thito) ;
+
     this.totalBranch = this.receipts.reduce( function(accumulator, currentValue){ return accumulator +  parseFloat(currentValue.allocated_amount)}, 0 ) ;
 
     this.totalGls = this.receipts_gls.reduce( function(accumulator, currentValue){ return accumulator +  parseFloat(currentValue.allocated_amount)}, 0 ) ;
     this.totalSun = this.receipts_sun.reduce( function(accumulator, currentValue){ return accumulator +  parseFloat(currentValue.allocated_amount)}, 0 ) ;
     this.totalTPOL = this.receipts_tpol.reduce( function(accumulator, currentValue){ return accumulator +  parseFloat(currentValue.allocated_amount)}, 0 ) ;
 
+    this.totalTPP = this.receipts_thito.reduce( function(accumulator, currentValue){ return accumulator +  parseFloat(currentValue.allocated_amount)}, 0 ) ; /// tpp
+
     this.totalGCount = this.receipts_gls.reduce( function(accumulator, currentValue){ return accumulator +  parseInt(currentValue.receipt_count)}, 0 ) ;
     this.totalPCount = this.receipts_tpol.reduce( function(accumulator, currentValue){ return accumulator +  parseInt(currentValue.receipt_count)}, 0 ) ;
     this.totalSCount = this.receipts_sun.reduce( function(accumulator, currentValue){ return accumulator +  parseInt(currentValue.receipt_count)}, 0 ) ;
 
+    this.totalTPPC = this.receipts_thito.reduce( function(accumulator, currentValue){ return accumulator +  parseInt(currentValue.receipt_count)}, 0 ) ; /// tpp
+
+    /// Nestle => flag to show payment method dynamically
+    if ( this.receipts_thito.length > 0  )
+    {
+      this.boxOffice = true ; 
+    }
   }
 
   toggleDisplayReport(){
     this.displayReport = !this.displayReport ; // false
   }
-  
+
+  /// Nestle => report container to show all apps dynamically
+
+  reciepts: any[]= [
+    {s_no:"",reciept_number:1234,reciept_date:"27/09/67",amount:"258896.00"},
+    {s_no:"",reciept_number:4567,reciept_date:"27/09/97",amount:"458253.00"},
+    {s_no:"",reciept_number:4867,reciept_date:"02/09/97",amount:"488253.00"},
+    {s_no:"",reciept_number:4567,reciept_date:"02/11/07",amount:"888253.00"},
+    {s_no:"",reciept_number:9897,reciept_date:"09/11/07",amount:"1888253.00"}
+  ];
+
+  groupies : any ;
+  groupies2 : any ;
+
+  showGroupies(){
+    console.log( _.groupBy(this.reciepts, "reciept_number") ) ;
+
+    this.groupies = _.groupBy(this.reciepts, "reciept_number") ;
+
+    this.groupies2 = JSON.stringify(this.groupies) ; 
+    
+    console.log( this.groupies2 ) ;
+  }
+
+  getSum(x: any): number {
+    // console.log(x) ;
+    return  x.reduce( function(accumulator, currentValue){ return accumulator +  parseFloat(currentValue.amount)}, 0 ) ;
+  }
+
+
 }
