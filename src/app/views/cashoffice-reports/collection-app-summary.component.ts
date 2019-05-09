04f2a4140c61e03f4deeb792f02c584fc7263574
kabelo-sais <45555892+kabelo-sais@users.noreply.github.com>
2019-05-09 11:32:41 +0200

    Minnie - removing unused code.

--- src/app/views/cashoffice-reports/collection-app-summary.component.ts
+++ src/app/views/cashoffice-reports/collection-app-summary.component.ts
@@ -34,20 +34,8 @@ export class CollectionAppSummaryComponent {
   displayReport = false ;
 
   receipts : any ;
-  receipts_gls : any ; // Group Life System
-  receipts_sun : any ; // Sundry Receipts
-  receipts_tpol : any ; // 
 
-  // Collection Receipt Counters
-  totalGCount : number = 0 ; 
-  totalPCount : number = 0 ; 
-  totalSCount : number = 0 ; 
-
-  // Collection Amount Totals
   totalBranch: number = 0.0 ; 
-  totalGls: number = 0.0 ; 
-  totalSun: number = 0.0 ; 
-  totalTPOL: number = 0.0 ; 
   
   url : string;
 
@@ -67,8 +55,6 @@ export class CollectionAppSummaryComponent {
 
     }
     ,err => this.handleError(err));
-
-    this.showGroupies() ;
   }
 
   detailReport(){
@@ -86,10 +72,6 @@ export class CollectionAppSummaryComponent {
     .subscribe((response)=>
     {
       this.receipts = response; 
-      
-      // this.cashOfficeCollection = this.receipts.reduce( function(accumulator, currentValue){ return accumulator +  parseFloat(currentValue.receipt_amount)}, 0 ) ;
-           
-      // this.branchCollection = this.receipts.reduce( function(accumulator, currentValue){ return accumulator +  parseFloat(currentValue.receipt_amount)}, 0 ) ;
     }
     ,err => this.handleError(err)
     , () => this.sums() 
@@ -99,9 +81,6 @@ export class CollectionAppSummaryComponent {
 
   }
 
-  // returns all items that have "filter" from the "list"
-  filterApp(list, filter) { return list.filter( e => e.app_desc.includes( filter ) ) ; }
-
   private handleError(error:Response){
     console.log(error);
     return Observable.throw('server error');
@@ -110,59 +89,30 @@ export class CollectionAppSummaryComponent {
   private sums(){
     this.branchName = this.receipts[0].branch_name ;
 
-    this.receipts_gls = this.filterApp(this.receipts, "GROUP LIFE") ;
-    this.receipts_sun = this.filterApp(this.receipts, "SUNDRY") ;
-    this.receipts_tpol = this.filterApp(this.receipts, "POLICY") ;
-
     this.totalBranch = this.receipts.reduce( function(accumulator, currentValue){ return accumulator +  parseFloat(currentValue.allocated_amount)}, 0 ) ;
-
-    this.totalGls = this.receipts_gls.reduce( function(accumulator, currentValue){ return accumulator +  parseFloat(currentValue.allocated_amount)}, 0 ) ;
-    this.totalSun = this.receipts_sun.reduce( function(accumulator, currentValue){ return accumulator +  parseFloat(currentValue.allocated_amount)}, 0 ) ;
-    this.totalTPOL = this.receipts_tpol.reduce( function(accumulator, currentValue){ return accumulator +  parseFloat(currentValue.allocated_amount)}, 0 ) ;
-
-    this.totalGCount = this.receipts_gls.reduce( function(accumulator, currentValue){ return accumulator +  parseInt(currentValue.receipt_count)}, 0 ) ;
-    this.totalPCount = this.receipts_tpol.reduce( function(accumulator, currentValue){ return accumulator +  parseInt(currentValue.receipt_count)}, 0 ) ;
-    this.totalSCount = this.receipts_sun.reduce( function(accumulator, currentValue){ return accumulator +  parseInt(currentValue.receipt_count)}, 0 ) ;
 
     this.showGroupies() ;
   }
 
   toggleDisplayReport(){
-    this.displayReport = !this.displayReport ; // false
+    this.displayReport = !this.displayReport ;
   }
-  
-   /// Nestle => report container to show all apps dynamically
-
-   reciepts: any[]= [
-    {s_no:"",reciept_number:1234,reciept_date:"27/09/67",amount:"258896.00"},
-    {s_no:"",reciept_number:4567,reciept_date:"27/09/97",amount:"458253.00"},
-    {s_no:"",reciept_number:4867,reciept_date:"02/09/97",amount:"488253.00"},
-    {s_no:"",reciept_number:4567,reciept_date:"02/11/07",amount:"888253.00"},
-    {s_no:"",reciept_number:9897,reciept_date:"09/11/07",amount:"1888253.00"}
-  ];
 
   groupies : any ;
-  groupies2 : any ;
 
   showGroupies(){
-    // console.log( _.groupBy(this.reciepts, "reciept_number") ) ;
-    console.log( _.groupBy(this.receipts, "app_desc") ) ;
-
-    // this.groupies = _.groupBy(this.reciepts, "reciept_number") ;
-    this.groupies = _.groupBy(this.receipts, "app_desc") ;
-
-    this.groupies2 = JSON.stringify(this.groupies) ; 
-    
-    console.log( this.groupies2 ) ;
+    // console.log( _.groupBy(this.receipts, "app_desc") ) ; // dbg
+    this.groupies = _.groupBy(_.sortBy(this.receipts, "app_desc"), "app_desc") ;
   }
 
   getCount(x: any): number {
-    // console.log(x) ;
+    // console.log(x) ; // dbg
     return  x.reduce( function(accumulator, currentValue){ return accumulator +  parseInt(currentValue.receipt_count)}, 0 ) ;
   }
 
   getSum(x: any): number {
-    // console.log(x) ;
+    // console.log(x) ; // dbg
     return  x.reduce( function(accumulator, currentValue){ return accumulator +  parseFloat(currentValue.allocated_amount)}, 0 ) ;
   }
+
 }
