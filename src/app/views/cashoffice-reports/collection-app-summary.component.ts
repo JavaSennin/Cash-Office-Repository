9cc43697593c24e776457e9bf0da54a413d52f91
kabelo-sais <45555892+kabelo-sais@users.noreply.github.com>
2019-05-09 11:02:33 +0200

    Display All Applications/Payment-Methods.

--- src/app/views/cashoffice-reports/collection-app-summary.component.ts
+++ src/app/views/cashoffice-reports/collection-app-summary.component.ts
@@ -24,11 +24,9 @@ export class CollectionAppSummaryComponent {
 
   detailInput = new FormGroup({
     branchCode: new FormControl('', Validators.required),
-    fromDate: new FormControl('2018-09-01', Validators.required),
-    toDate: new FormControl('2018-09-30', Validators.required)
+    fromDate: new FormControl('2019-01-01', Validators.required),
+    toDate: new FormControl('2019-01-31', Validators.required)
   });
-
-  boxOffice : boolean = false ; 
 
   branchCodes : any
   branchName: string = "" ;  
@@ -38,21 +36,18 @@ export class CollectionAppSummaryComponent {
   receipts : any ;
   receipts_gls : any ; // Group Life System
   receipts_sun : any ; // Sundry Receipts
-  receipts_tpol : any ; // Polity
-  receipts_thito : any ; /// Thito Pay Point 
+  receipts_tpol : any ; // 
 
   // Collection Receipt Counters
   totalGCount : number = 0 ; 
   totalPCount : number = 0 ; 
-  totalSCount : number = 0 ;
-  totalTPPC : number = 0 ; /// tpp 
+  totalSCount : number = 0 ; 
 
   // Collection Amount Totals
   totalBranch: number = 0.0 ; 
   totalGls: number = 0.0 ; 
   totalSun: number = 0.0 ; 
   totalTPOL: number = 0.0 ; 
-  totalTPP : number = 0.0 ; /// tpp
   
   url : string;
 
@@ -73,7 +68,7 @@ export class CollectionAppSummaryComponent {
     }
     ,err => this.handleError(err));
 
-    this.showGroupies() ; /// dbg
+    this.showGroupies() ;
   }
 
   detailReport(){
@@ -119,37 +114,26 @@ export class CollectionAppSummaryComponent {
     this.receipts_sun = this.filterApp(this.receipts, "SUNDRY") ;
     this.receipts_tpol = this.filterApp(this.receipts, "POLICY") ;
 
-    this.receipts_thito = this.filterApp(this.receipts, "THITO") ; /// tpp
-    console.log(this.receipts_thito) ;
-
     this.totalBranch = this.receipts.reduce( function(accumulator, currentValue){ return accumulator +  parseFloat(currentValue.allocated_amount)}, 0 ) ;
 
     this.totalGls = this.receipts_gls.reduce( function(accumulator, currentValue){ return accumulator +  parseFloat(currentValue.allocated_amount)}, 0 ) ;
     this.totalSun = this.receipts_sun.reduce( function(accumulator, currentValue){ return accumulator +  parseFloat(currentValue.allocated_amount)}, 0 ) ;
     this.totalTPOL = this.receipts_tpol.reduce( function(accumulator, currentValue){ return accumulator +  parseFloat(currentValue.allocated_amount)}, 0 ) ;
 
-    this.totalTPP = this.receipts_thito.reduce( function(accumulator, currentValue){ return accumulator +  parseFloat(currentValue.allocated_amount)}, 0 ) ; /// tpp
-
     this.totalGCount = this.receipts_gls.reduce( function(accumulator, currentValue){ return accumulator +  parseInt(currentValue.receipt_count)}, 0 ) ;
     this.totalPCount = this.receipts_tpol.reduce( function(accumulator, currentValue){ return accumulator +  parseInt(currentValue.receipt_count)}, 0 ) ;
     this.totalSCount = this.receipts_sun.reduce( function(accumulator, currentValue){ return accumulator +  parseInt(currentValue.receipt_count)}, 0 ) ;
 
-    this.totalTPPC = this.receipts_thito.reduce( function(accumulator, currentValue){ return accumulator +  parseInt(currentValue.receipt_count)}, 0 ) ; /// tpp
-
-    /// Nestle => flag to show payment method dynamically
-    if ( this.receipts_thito.length > 0  )
-    {
-      this.boxOffice = true ; 
-    }
+    this.showGroupies() ;
   }
 
   toggleDisplayReport(){
     this.displayReport = !this.displayReport ; // false
   }
+  
+   /// Nestle => report container to show all apps dynamically
 
-  /// Nestle => report container to show all apps dynamically
-
-  reciepts: any[]= [
+   reciepts: any[]= [
     {s_no:"",reciept_number:1234,reciept_date:"27/09/67",amount:"258896.00"},
     {s_no:"",reciept_number:4567,reciept_date:"27/09/97",amount:"458253.00"},
     {s_no:"",reciept_number:4867,reciept_date:"02/09/97",amount:"488253.00"},
@@ -161,19 +145,24 @@ export class CollectionAppSummaryComponent {
   groupies2 : any ;
 
   showGroupies(){
-    console.log( _.groupBy(this.reciepts, "reciept_number") ) ;
+    // console.log( _.groupBy(this.reciepts, "reciept_number") ) ;
+    console.log( _.groupBy(this.receipts, "app_desc") ) ;
 
-    this.groupies = _.groupBy(this.reciepts, "reciept_number") ;
+    // this.groupies = _.groupBy(this.reciepts, "reciept_number") ;
+    this.groupies = _.groupBy(this.receipts, "app_desc") ;
 
     this.groupies2 = JSON.stringify(this.groupies) ; 
     
     console.log( this.groupies2 ) ;
   }
 
-  getSum(x: any): number {
+  getCount(x: any): number {
     // console.log(x) ;
-    return  x.reduce( function(accumulator, currentValue){ return accumulator +  parseFloat(currentValue.amount)}, 0 ) ;
+    return  x.reduce( function(accumulator, currentValue){ return accumulator +  parseInt(currentValue.receipt_count)}, 0 ) ;
   }
 
-
+  getSum(x: any): number {
+    // console.log(x) ;
+    return  x.reduce( function(accumulator, currentValue){ return accumulator +  parseFloat(currentValue.allocated_amount)}, 0 ) ;
+  }
 }
