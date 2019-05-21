36c7aa988386253565778102c2985390dafd89fe
JavaSennin <iamt4@live.com>
2019-05-21 09:33:48 +0200

    Get

--- src/app/views/cashoffice-reports/cashier-assignment.component.ts
+++ src/app/views/cashoffice-reports/cashier-assignment.component.ts
@@ -1,6 +1,5 @@
-import { Component,NgModule, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms'; 
-import { NgxXml2jsonService } from 'ngx-xml2json'; 
+import { Component, NgModule, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
@@ -8,176 +7,284 @@ import { Observable } from 'rxjs';
 @NgModule({
   imports: [
     FormControl,
-    FormGroup, 
+    FormGroup,
     FormsModule,
     ReactiveFormsModule,
     Validators,
     HttpClient,
-]
+  ]
 })
 
 @Component({
   templateUrl: './cashier-assignment.component.html'
 })
-export class CashierAssignmentComponent {
+export class CashierAssignmentComponent implements OnInit {
 
   cashierInput = new FormGroup({
     branchCode: new FormControl('', Validators.required),
-    cashOfficeCode: new FormControl('', Validators.required)
+    cashOfficeCode: new FormControl('', Validators.required),
   });
-  url : string;
-  branchCodes :any;
-  cashCodes :any;
-  paymentMethod:any;
-  reportNumber:Number;
-  branchName:string;
-  applications:any;
-  cashiers:any;
+  url: string;
+  branchCodes: any;
+  cashCodes: any;
+  paymentMethod: any;
+  reportNumber: Number;
+  branchName: string;
+  cashOfficeDesc: string;
+  applications: any;
+  cashiers: any;
+  allReports: any;
 
-  constructor(private http:HttpClient){
-
-  }
-  
- 
-  ngOnInit(){
-    
-  
-    const httpOptions ={
-      headers : new HttpHeaders({'Content-Type':'application/json','responseType':'application/json'})
-     }
-   this.url ="http://localhost:8080/cash/cashier-assignment/branchCodes"
-   this.http.get(this.url,httpOptions)
-    .subscribe((response)=>{
-      const obj = response;
-      
-      this.branchCodes = obj; 
-      
-
-    }
-    ,err => this.handleError(err));
-   
-    
-  }
-getCashCodes(){
-
-  let bc  = this.cashierInput.get('branchCode').value;
-  const httpOptions ={
-    headers : new HttpHeaders({'Content-Type':'application/json','responseType':'application/json'})
-   }
- this.url ="http://localhost:8080/cash/cashier-assignment/CashCodes/"+bc;
- this.http.get(this.url,httpOptions)
-  .subscribe((response)=>{
-    const obj = response;
-    console.log(obj);
-    this.cashCodes = obj; 
-    console.log(this.cashCodes);
+  constructor(private http: HttpClient) {
 
   }
-  ,err => this.handleError(err));
- 
 
-}
-  private handleError(error:Response){
-    console.log(error);
-    return Observable.throw('server error');
-  }
-  
-
-  displayReport = false ;
-
-  toggleDisplayReport(){
-    this.displayReport = !this.displayReport ; // false
-  }
-  
-  // An Method to hold dynamic data - Payment Methods: 
- getPaymentMethods(){
-
-  let bc  = this.cashierInput.get('branchCode').value;
-  let co  = this.cashierInput.get('cashOfficeCode').value;
-  const httpOptions ={
-    headers : new HttpHeaders({'Content-Type':'application/json','responseType':'application/json'})
-   }
- this.url ="http://localhost:8080/cash/cashier-assignment/paymentMethods/"+bc+"&"+co;
- this.http.get(this.url,httpOptions)
-  .subscribe((response)=>{
-    const obj = response;
-   
-    this.paymentMethod = obj; 
-    this.branchName = this.paymentMethod[1][4];
-
-    
-
-  }
-  ,err => this.handleError(err));
- 
-
-
- }
-   // An Method to hold dynamic data - Application: 
- getApplication(){
-
-  let bc  = this.cashierInput.get('branchCode').value;
-  let co  = this.cashierInput.get('cashOfficeCode').value;
-  const httpOptions ={
-    headers : new HttpHeaders({'Content-Type':'application/json','responseType':'application/json'})
-   }
- this.url ="http://localhost:8080/cash/cashier-assignment/application/"+bc+"&"+co;
- this.http.get(this.url,httpOptions)
-  .subscribe((response)=>{
-    const obj = response;
-   
-    this.applications = obj; 
-   
-
-    
-
-  }
-  ,err => this.handleError(err));
- 
-
-
- }
- getCashiers(){
-
-  let bc  = this.cashierInput.get('branchCode').value;
-  let co  = this.cashierInput.get('cashOfficeCode').value;
-  const httpOptions ={
-    headers : new HttpHeaders({'Content-Type':'application/json','responseType':'application/json'})
-   }
- this.url ="http://localhost:8080/cash/cashier-assignment/cashier/"+bc+"&"+co;
- this.http.get(this.url,httpOptions)
-  .subscribe((response)=>{
-    const obj = response;
-   
-    this.cashiers = obj; 
-   
-
-    
-
-  }
-  ,err => this.handleError(err));
- 
-
-
- }  
- 
- 
-
+  displayAll = false;
+  displayReport = false;
   currentDate = new Date();
 
-  randomInt(){
-    let max=10000000;
-    let min=1346;
-    
-    this.reportNumber =Math.floor(Math.random() * (max - min + 1)) + min;
- }
- 
-  onSubmit(){
-    this.getPaymentMethods();
-    this.getApplication();
-    this.getCashiers();
-    this.displayReport = true ; // show container for the results
-   
+  ngOnInit() {
+
+
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json', 'responseType': 'application/json' }),
+    };
+    this.url = 'http://localhost:8080/cash/cashier-assignment/branchCodes';
+    this.http.get(this.url, httpOptions)
+      .subscribe((response) => {
+        const obj = response;
+
+        this.branchCodes = obj;
+
+
+      }
+        , err => this.handleError(err));
+
+
+  }
+  getCashCodes() {
+
+    const bc = this.cashierInput.get('branchCode').value;
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json', 'responseType': 'application/json' })
+    };
+    this.url = 'http://localhost:8080/cash/cashier-assignment/CashCodes/' + bc;
+    this.http.get(this.url, httpOptions)
+      .subscribe((response) => {
+        const obj = response;
+        console.log(obj);
+        this.cashCodes = obj;
+        console.log(this.cashCodes);
+
+      }
+        , err => this.handleError(err));
+
+
+  }
+  private handleError(error: Response) {
+    console.log(error);
+    return 'Erorr';
+  }
+
+
+  toggleDisplayReport() {
+    this.displayReport = !this.displayReport; // false
+  }
+  selectMeth() {
+
+  }  // An Method to hold dynamic data - Payment Methods:
+  getPaymentMethods() {
+
+    const bc = this.cashierInput.get('branchCode').value;
+    const co = this.cashierInput.get('cashOfficeCode').value;
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json', 'responseType': 'application/json' })
+    };
+    this.url = 'http://localhost:8080/cash/cashier-assignment/paymentMethods/' + bc + '&' + co;
+    this.http.get(this.url, httpOptions)
+      .subscribe((response) => {
+        const obj = response;
+        console.log(obj);
+        this.paymentMethod = obj;
+        this.branchName = this.paymentMethod[0].branch_name;
+        this.cashOfficeDesc = this.paymentMethod[0].cash_office_desc;
+
+
+
+      }
+        , err => this.handleError(err));
+
+
+
+  }
+  getAllPaymentMethods() {
+
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json', 'responseType': 'application/json' })
+    };
+    this.url = 'http://localhost:8080/cash/cashier-assignment/allpaymentMethods';
+    this.http.get(this.url, httpOptions)
+      .subscribe((response) => {
+        const obj = response;
+        console.log(obj);
+        this.paymentMethod = obj;
+        this.branchName = this.paymentMethod[0].branch_name;
+        this.cashOfficeDesc = this.paymentMethod[0].cash_office_desc;
+
+
+
+      }
+        , err => this.handleError(err));
+
+
+
+  }
+
+  // An Method to hold dynamic data - Application:
+  getApplication() {
+
+    const bc = this.cashierInput.get('branchCode').value;
+    const co = this.cashierInput.get('cashOfficeCode').value;
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json', 'responseType': 'application/json' })
+    };
+    this.url = 'http://localhost:8080/cash/cashier-assignment/application/' + bc + '&' + co;
+    this.http.get(this.url, httpOptions)
+      .subscribe((response) => {
+        const obj = response;
+
+        this.applications = obj;
+
+
+
+
+      }
+        , err => this.handleError(err));
+
+
+
+  }
+  getAllApplication() {
+
+    const bc = this.cashierInput.get('branchCode').value;
+    const co = this.cashierInput.get('cashOfficeCode').value;
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json', 'responseType': 'application/json' })
+    };
+    this.url = 'http://localhost:8080/cash/cashier-assignment/application/' + bc + '&' + co;
+    this.http.get(this.url, httpOptions)
+      .subscribe((response) => {
+        const obj = response;
+
+        this.applications = obj;
+
+
+
+
+      }
+        , err => this.handleError(err));
+
+
+
+  }
+  getCashiers() {
+
+    const bc = this.cashierInput.get('branchCode').value;
+    const co = this.cashierInput.get('cashOfficeCode').value;
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json', 'responseType': 'application/json' })
+    };
+    this.url = 'http://localhost:8080/cash/cashier-assignment/cashier/' + bc + '&' + co;
+    this.http.get(this.url, httpOptions)
+      .subscribe((response) => {
+        const obj = response;
+
+        this.cashiers = obj;
+
+
+
+
+      }
+        , err => this.handleError(err));
+
+
+
+  }
+
+  getAllCashiers() {
+
+    const bc = this.cashierInput.get('branchCode').value;
+    const co = this.cashierInput.get('cashOfficeCode').value;
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json', 'responseType': 'application/json' })
+    }
+    this.url = 'http://localhost:8080/cash/cashier-assignment/cashier/' + bc + '&' + co;
+    this.http.get(this.url, httpOptions)
+      .subscribe((response) => {
+        const obj = response;
+
+        this.cashiers = obj;
+
+
+
+
+      }
+        , err => this.handleError(err));
+
+
+
+  }
+
+
+
+
+
+  randomInt() {
+    let max = 10000000;
+    let min = 1346;
+
+    this.reportNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  getAllreports() {
+
+    this.displayAll = true;
+
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json', 'responseType': 'application/json' })
+    };
+    this.url = 'http://localhost:8080/cash/cashier-assignment/allreports';
+    this.http.get(this.url, httpOptions)
+      .subscribe((response) => {
+        const obj = response;
+        this.allReports = obj;
+        console.log(this.allReports);
+
+
+      }
+        , err => this.handleError(err));
+
+
+  }
+
+  onSubmit() {
+
+    const x: number = this.cashierInput.get('branchCode').value.length;
+    const y: number = this.cashierInput.get('cashOfficeCode').value.length;
+
+    if (y < 1) {
+
+      console.log('Please Complete all fields')
+      this.displayReport = !this.displayReport;
+      // show container for the results
+    } else {
+      console.log(y);
+      this.getPaymentMethods();
+      this.getApplication();
+      this.getCashiers();
+      this.displayReport = true;
+      // this.cashierInput.disabled;
+    }
 
   }
 }
