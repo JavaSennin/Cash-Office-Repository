bb36f7ef808aad033f8e9bae3359da763ed16615
JavaSennin <iamt4@live.com>
2019-06-21 15:43:56 +0200

    Commit Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
 import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
-// import { HttpClientModule } from '@angular/common/http'; 
+// import { HttpClientModule } from '@angular/common/http';
 
 const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
   suppressScrollX: true
@@ -21,20 +21,20 @@ import { P500Component } from './views/error/500.component';
 import { LoginComponent } from './views/login/login.component';
 import { RegisterComponent } from './views/register/register.component';
 import { NgxSpinnerModule } from 'ngx-spinner';
-import {NgxPrintModule} from 'ngx-print';
+import { NgxPrintModule } from 'ngx-print';
 
 const APP_CONTAINERS = [
   DefaultLayoutComponent
-] ;
+];
 
 import {
   AppAsideModule,
   AppBreadcrumbModule,
   AppHeaderModule,
   AppFooterModule,
-  AppSidebarModule,
- 
- 
+  AppSidebarModule
+
+
 } from '@coreui/angular';
 
 // Import routing module
@@ -44,14 +44,18 @@ import { AppRoutingModule } from './app.routing';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 // Web Service Things
-import { HttpClientModule,  } from '@angular/common/http';
+import { HttpClientModule, } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
 
-import { PagerService,GlobalServices } from './services/index';
+import { PagerService, GlobalServices } from './services/index';
+import { ModalService } from './services/_service/index';
 
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LoadingSpinnerComponent } from './ui/loading-spinner/loading-spinner.component';
+
 
 @NgModule({
   imports: [
@@ -68,15 +72,19 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
     ChartsModule,
     HttpClientModule,
     NgxPrintModule,
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    NgbModule
+
   ],
+  exports: [LoadingSpinnerComponent],
   declarations: [
     AppComponent,
     ...APP_CONTAINERS,
     P404Component,
     P500Component,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoadingSpinnerComponent
   ],
   providers: [
     {
@@ -84,9 +92,12 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
       useClass: HashLocationStrategy
     }
 
-    , PagerService, GlobalServices // from Ralla
+    , PagerService, GlobalServices, ModalService// from Ralla
 
   ],
-  bootstrap: [ AppComponent ]
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
