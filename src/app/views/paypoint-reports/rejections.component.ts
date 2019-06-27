a08b55a9e66172369f4532a206bc30131ce8b57a
JavaSennin <iamt4@live.com>
2019-06-27 09:39:47 +0200

    Rejections

--- src/app/views/paypoint-reports/rejections.component.ts
+++ src/app/views/paypoint-reports/rejections.component.ts
@@ -1,92 +1,142 @@
-import { Component, NgModule } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators, FormBuilder } from '@angular/forms'; 
+import { Component, NgModule, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators, FormBuilder } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import * as _ from 'underscore'; /// npm install underscore
 
 @NgModule({
   imports: [
     FormControl,
-    FormGroup, 
+    FormGroup,
     FormsModule,
     ReactiveFormsModule,
     Validators
-    
-]
+
+  ]
 })
 
 @Component({
-  templateUrl: 'rejections.component.html'
+  templateUrl: 'rejections.component.html',
+  styleUrls: ['./spinner.component.scss']
 })
-export class rejectionsComponent {
+export class rejectionsComponent implements OnInit {
 
   detailInput = new FormGroup({
     PayPointID: new FormControl('', Validators.required),
-    
-    Paypoint_Name: new FormControl({value:"", disabled: true}, Validators.required),
-    Period: new FormControl('2018-09-30', Validators.required)
+    Paypoint_Name: new FormControl(''),
+    Period: new FormControl('', Validators.required)
   });
- 
-
-  detailReport(){
-    this.detailInput.disable() ;
-    this.displayReport = true ;
-    
-    console.table(this.detailInput.value) ;
-
-    // form-processing code
-  }
-  displayReport = false ;
+  paypointIds: any;
+  paypointName: any;
+  error_message: any;
+  err: any;
+  report: any;
+  url: any;
+  groupies: any;
+  showSpinner: boolean;
+  total: number;
+  totalCred: number;
+  displayReport = false;
 
   disableForm = false;
+  constructor(private http: HttpClient) { }
 
-  toggleDisplayReport(){
-    this.displayReport = !this.displayReport ; // false
-    
-    this.detailInput.enable() ;
-    this.detailInput.get('Paypoint_Name').disable();
-    
-    
+  ngOnInit() {
+
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json', 'responseType': 'application/json' }),
+    };
+    this.url = 'http://localhost:8080/cash/rejections/paypoints';
+    this.http.get(this.url, httpOptions)
+      .subscribe((response) => {
+        const obj = response;
+
+        this.paypointIds = obj;
+        this.error_message = false;
+
+
+      }
+        ,
+        (err) => {
+          this.err = 'Server unreachable | Check if ts running |  available branch codes not retrieved';
+          this.error_message = true;
+          this.displayReport = false;
+        });
   }
-  
-  // paypointIds: any[]= [
-  paypointIds = [ 
-    {ppID:1234, ppName: "Botswana Railways"},
-    {ppID:4567, ppName: "Botswana Post"},
-    {ppID:4867, ppName: "Botswana Meat Commission"},
-    {ppID:8897, ppName: "Botswana Life"},
-    {ppID:9897, ppName: "Hollard Insurance"}
-  ];
+  getPaypointName() {
+    const ppID = this.detailInput.get('PayPointID').value;
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json', 'responseType': 'application/json' })
+    };
+    this.url = 'http://localhost:8080/cash/rejections/paypointname/' + ppID;
+    this.http.get(this.url, httpOptions)
+      .subscribe((response) => {
 
-  // PayPointID1 = this.paypointIds[2] ;
 
-  //Array for Dummy data [Group Life System]
-  ppReport: any[]= [
-    {pp_id:1234,ppType:"Employer",pMode:"ESO(Semi-Electronic"}
-  ];
-   //Array for Dummy data
-   ppReport2: any[]= [
-    {collectionType:"Debits:",Total:234456.67},
-    {collectionType:"Credits:",Total:0}
-    
-  ];
-  ppReport3: any[]= [
-    {ref_no:"00",ref_name:"Nonofo Odubegile",party_id:"445889600",policy_no:"987564896",product_code:"BMFW-1",policy_status:"inforce",dramount:2121.58},
-    {ref_no:"01",ref_name:"Kate Badubi",party_id:"445589600",policy_no:"987564476",product_code:"UML2-1",policy_status:"inforce",dramount:2121.58},
-    
-    
-  ];
+        this.paypointName = response;
+      }, (err) => {
 
-  // bgn
+        this.err = 'No Cash Office Codes';
+        this.error_message = true;
+        this.displayReport = false;
+      });
+  }
 
-  // states = [
-  //   {name: 'Arizona', abbrev: 'AZ'},
-  //   {name: 'California', abbrev: 'CA'},
-  //   {name: 'Colorado', abbrev: 'CO'},
-  //   {name: 'New York', abbrev: 'NY'},
-  //   {name: 'Pennsylvania', abbrev: 'PA'},
-  // ];
- 
-  // form = new FormGroup({
-  //   state: new FormControl(this.states[3]),
-  // });
+  getReport() {
 
-  // end
+    this.showSpinner = true;
+    const ppID = this.detailInput.get('PayPointID').value;
+    const period = this.detailInput.get('Period').value;
+    console.log(period);
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json', 'responseType': 'application/json' })
+    };
+    this.url = 'http://localhost:8080/cash/rejections/report/' + ppID + '&' + period;
+    this.http.get(this.url, httpOptions)
+      .subscribe((response) => {
+
+        this.report = response;
+        if (this.report.length == 0) // Error handling. Put all-else in ELSE part
+        {
+          this.error_message = true;
+          this.showSpinner = false;
+          this.err = "No Matching Data Found";
+          this.displayReport = false;
+        } else {
+          this.totalDebitsAndCredits();
+          this.showSpinner = false;
+          this.error_message = false;
+          this.displayReport = true;
+
+        }
+      }, (err) => {
+
+        this.err = 'No Cash Office Codes';
+        this.error_message = true;
+        this.displayReport = false;
+      },
+        () => this.sums());
+
+  }
+
+  sums() {
+    this.showGroupies();
+  }
+  showGroupies() {
+    this.groupies = _.groupBy(this.report, 'paypoint_ID'); // or sort cash_office_desc
+  }
+
+
+  totalDebitsAndCredits() {
+    this.total = this.report.reduce(function (accumulator, currentValue) { return accumulator + parseFloat(currentValue.amount) }, 0);
+    this.totalCred = this.report.reduce(function (accumulator, currentValue) { return accumulator + parseFloat(currentValue.credit_AMOUNT) }, 0);
+
+  }
+
+
+  toggleDisplayReport() {
+    this.displayReport = false;
+    this.detailInput.reset();
+  }
+
+
 }
